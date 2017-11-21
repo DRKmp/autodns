6a6d1290c6f6bc8115a408db6f02c1931f27d63e
DRKmp <33774413+DRKmp@users.noreply.github.com>
2017-11-21 17:17:09 -0200

    Update autoip_change.py

--- Auto DNS/func/autoip_change.py
+++ Auto DNS/func/autoip_change.py
@@ -7,100 +7,127 @@ from time import sleep
 def novaLinha():
     print('')
 
+class autochange(object):
 #Função para fazer backup do arquivo de configurações DNS localizado em
 ## '/etc/resolv.conf'
-def mkbak(yon):
-
+    def mkbak(yon):
 #Se fazer backup for igual a 'sim':
-    while True:
-        if yon == 'y' or yon == 'Y':
-            system('echo > ~/resolv.conf.bakAutoDns')
-            system('cp /etc/resolv.conf ~/resolv.conf.bakAutoDns')
-            system("echo '#Backup gerado pelo Auto DNS' >> ~/resolv.conf.bakAutoDns")
-            print('\nBackup salvo na sua /home')
-            sleep(2)
-            return False
-            continue
+        while True:
+            print('\033[1;32m')
+            if yon == 'y' or yon == 'Y':
+                system('echo > ~/resolv.conf.bakAutoDns')
+                system('cp /etc/resolv.conf ~/resolv.conf.bakAutoDns')
+                system("echo '#Backup gerado pelo Auto DNS' >> ~/resolv.conf.bakAutoDns")
+                print('\nBackup salvo na sua /home')
+                sleep(2)
+                return False
+                continue
 
-        if yon == 'n' or yon == 'N':
-            system("clear")
-            print('Ok! Prosseguindo...')
-            sleep(2)
-            return False
-            continue
-        elif yon != 'y' or yon != 'Y':
-            print('Opção inválida! Considerando [y].')
-            yon = 'y'
-            sleep(3)
-            return False
-            continue
-        elif yon != 'n' or yon != 'N':
-            print('Opção inválida! Considerando [y].')
-            yon = 'y'
-            sleep(3)
-            return False
-            continue
+            if yon == 'n' or yon == 'N':
+                system("clear")
+                print('Ok! Prosseguindo...')
+                sleep(2)
+                return False
+                continue
+            elif yon != 'y' or yon != 'Y':
+                print('Opção inválida! Considerando [y].')
+                yon = 'y'
+                sleep(3)
+                return False
+                continue
+            elif yon != 'n' or yon != 'N':
+                print('Opção inválida! Considerando [y].')
+                yon = 'y'
+                sleep(3)
+                return False
+                continue
 
 #Função para verificar se deseja fazer backup e setar as configurações
-def dnsserver(valor):
+    def dnsserver(valor):
 #Verifica se deseja realizar backup do arquivo 'resolv.conf' em '/etc'. 
 #Chama a função 'mkbak' passando o valor de 'bak' como argumento.
-    system("clear")
-    print('-=-'*25)
-    bak = str(input('Deseja fazer um backup agora? *O backup será feito em sua /home'+'\n'+'-=-'*25 +'\n[y/n]: '))
-    mkbak(yon = bak)
+        system("clear")
+        print('\033[1;32m')
+        print('-=-'*25)
+        bak = str(input('Deseja fazer um backup agora? *O backup será feito em sua /home'+'\n'+'-=-'*25 +'\n[y/n]: '))
+        autochange.mkbak(yon = bak)
+#Adiciona os valores correspondentes a opção selecionada no menu.
+        if valor == '1':
+            dns1 = '8.8.8.8'
+            dns2 = '8.8.4.4'
+            dns_n = 'Google'
 
-#Adiciona os valores conrrespondentes a opção selecionada no menu.
-    if valor == '1':
-        dns1 = '8.8.8.8'
-        dns2 = '8.8.4.4'
-        dns_n = 'Google'
+        if valor == '2':
+            dns1 = '4.2.2.1'
+            dns2 = '4.2.2.2'
+            dns_n = 'Level3'
 
-    if valor == '2':
-        dns1 = '4.2.2.1'
-        dns2 = '4.2.2.2'
-        dns_n = 'Level3'
+        if valor == '3':
+            dns1 = '200.252.98.162'
+            dns2 = ''
+            dns_n = 'OpenNIC'
 
-    if valor == '3':
-        dns1 = '200.252.98.162'
-        dns2 = ''
-        dns_n = 'OpenNIC'
+        if valor == '4':
+            dns1 = '208.67.222.222'
+            dns2 = '208.67.220.220'
+            dns_n = 'OpenDns.org'
 
-    if valor == '4':
-        dns1 = '208.67.222.222'
-        dns2 = '208.67.220.220'
-        dns_n = 'OpenDns.org'
+        if valor == '5':
+            dns1 = '189.38.95.95'
+            dns2 = '189.38.95.96'
+            dns_n = 'GigaDns.com.br'
 
-    if valor == '5':
-        dns1 = '189.38.95.95'
-        dns2 = '189.38.95.96'
-        dns_n = 'GigaDns.com.br'
+        if valor == '6':
+            dns1 = '67.138.54.100'
+            dns2 = '207.225.209.66'
+            dns_n = 'ScrubIt'
 
-    if valor == '6':
-        dns1 = '67.138.54.100'
-        dns2 = '207.225.209.66'
-        dns_n = 'ScrubIt'
+        if valor == '7':
+            dns1 = '156.154.70.1'
+            dns2 = ''
+            dns_n = 'Dns Advantage'
 
-    if valor == '7':
-        dns1 = '156.154.70.1'
-        dns2 = ''
-        dns_n = 'Dns Advantage'
+        if valor == '8':
+            dns1 = '127.0.0.1'
+            dns2 = '201.10.120.2'
+            dns_n = 'Brasil Telecom'
+
+        if valor == '9':
+            dns1 = '200.255.121.39'
+            dns2 = '200.169.117.14'
+            dns_n = 'CLARO'
+
+        if valor == '10':
+            dns1 = '200.225.197.3'
+            dns2 = '200.225.197.37'
+            dns_n = 'CTBC'
+
+        if valor == '11':
+            dns1 = '127.0.0.1'
+            dns2 = '201.10.120.2'
+            dns_n = 'OI'
+
+        if valor == '12':
+            dns1 = '208.67.222.222'
+            dns2 = '208.67.220.220'
+            dns_n = 'TIM'
+
 ###
 
 #Utiliza os valores recebidos nas variáveis 'dns1', 'dns2', 'dns_n' e adiciona ao arquivo de configurações DNS
 ##localizado em 'etc/resolv.conf'
-    system("clear")
-    print('-=-'*20)
-    print("""DNS escolhido: {}
+        system("clear")
+        print('-=-'*20)
+        print("""DNS escolhido: {}
 IP: {} e {}""".format(dns_n, dns1, dns2))
-    system('echo > /etc/resolv.conf') #Apaga tudo dentro de /etc/resolv.conf
-    system("echo '#Gerado por Auto DNS' >> /etc/resolv.conf") #Créditos
-    system("echo '#{} DNS' >> /etc/resolv.conf".format(dns_n)) #Comenta o DNS usado
-    system("echo 'nameserver {}' >> /etc/resolv.conf".format(dns1)) #DNS Primário
-    system("echo 'nameserver {}' >> /etc/resolv.conf".format(dns2)) #DNS Secundário
-    novaLinha()
-    print('DNS {} configurado!'.format(dns_n))
-    print('Boa navegação!')
-    print('-=-'*20)
-    input('Pressione <enter> para voltar ao menu principal...')
+        system('echo > /etc/resolv.conf') #Apaga tudo dentro de /etc/resolv.conf
+        system("echo '#Gerado por Auto DNS' >> /etc/resolv.conf") #Créditos
+        system("echo '#{} DNS' >> /etc/resolv.conf".format(dns_n)) #Comenta o DNS usado
+        system("echo 'nameserver {}' >> /etc/resolv.conf".format(dns1)) #DNS Primário
+        system("echo 'nameserver {}' >> /etc/resolv.conf".format(dns2)) #DNS Secundário
+        novaLinha()
+        print('DNS {} configurado!'.format(dns_n))
+        print('Boa navegação!')
+        print('-=-'*20)
+        input('Pressione <enter> para voltar ao menu principal...')
 ###
